d52701c5f8d673f5145633f284fcc978dd0fa0d2
Evgeny Maslov <lyriccoder@gmail.com>
2020-09-21 13:46:00 +0300

    Fix comments

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from pathlib import Path
 
 def Precommit():
     if (True):
-        print(Path(".") + " 11111" 
-        + "22222")
-    
- 
- 
+        print(Path(".") + " 11111" + "22222")
+
 Precommit()
