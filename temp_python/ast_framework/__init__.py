6aaec808b02f7b993a99c32385ead3c93e9abbba
Evgeny Maslov <lyriccoder@gmail.com>
2020-09-22 16:20:17 +0300

    Fix init

--- temp_python/ast_framework/__init__.py
+++ temp_python/ast_framework/__init__.py
@@ -1,9 +1,9 @@
-from aibolit.ast_framework.ast_node_type import ASTNodeType  # noqa: F401
-from aibolit.ast_framework.ast_node import ASTNode  # noqa: F401
-from aibolit.ast_framework.ast import AST  # noqa: F401
+from temp_python.ast_framework.ast_node_type import ASTNodeType  # noqa: F401
+from temp_python.ast_framework.ast_node import ASTNode  # noqa: F401
+from temp_python.ast_framework.ast import AST  # noqa: F401
 
 # register all standard computed fields from 'computed_fields_catalog'
-from aibolit.ast_framework.computed_fields_catalog.standard_fields import (
+from temp_python.ast_framework.computed_fields_catalog.standard_fields import (
     register_standard_computed_properties,
 )
 
